17ecfffd54e74ad17e12d5ba0d7ab5c2fabae4ed
Ilia <ilya@s1q.ru>
2024-01-23 15:06:35 +0300

    Rewrote SQL queries and optimized the script

--- app.py
+++ app.py
@@ -1,54 +1,41 @@
-import pandas as pd
-import random
 import streamlit as st
 from db_connection import Db
 
 
 st.set_page_config(page_title='Speaking Club Material')
 database = Db()
-
-
-def get_club_data():
-    data = database.get_club_topics()
-    expressions = database.get_expressions()
-    return data, expressions
-
-
-def pick_random(df):
-    qs = pd.unique(df['questions'])
-    random_q = qs[random.randint(0, len(qs) - 1)]
-    filtered = df.query('questions == @random_q').reset_index(drop=True)
-    topic = filtered['topic'][0]
-    responses = filtered['answers']
-    rand_r = responses[random.randint(0, len(responses) - 1)]
-    return random_q, rand_r, topic
+pars_per_page = 5
 
 
 def main():
+    columns = st.columns(2)
+    for each in columns:
+        sub_cols = each.columns(3)
+        for each1 in sub_cols:
+            each1.write('1')
 
-    refresh = st.button('Refresh', use_container_width=True)
+    refresh = st.button('Get Some Speaking Material', use_container_width=True)
     if refresh:
-        st.rerun()
-    data, expressions = get_club_data()
-    for x in range(5):
-        rq, rr, rand_top = pick_random(data)
-        st.write(f"----")
-        st.markdown(f"<i>{rand_top.replace('_', ' ')}</i>", unsafe_allow_html=True)
-        st.subheader(f"{rq}")
-        st.write(f"")
-        st.write(f"{rr}")
-        st.write(f"")
-        exp_list = random.sample(expressions.query('topic == @rand_top')['expressions'].to_list(), 20)
-        col1, col2, col3, col4 = st.columns(4)
-        for i, each in enumerate(exp_list):
-            if i % 4 == 0:
-                col1.markdown(f"<i>{each}</i>", unsafe_allow_html=True)
-            elif i % 4 == 1:
-                col2.markdown(f"<i>{each}</i>", unsafe_allow_html=True)
-            elif i % 4 == 2:
-                col3.markdown(f"<i>{each}</i>", unsafe_allow_html=True)
-            else:
-                col4.markdown(f"<i>{each}</i>", unsafe_allow_html=True)
+        data = database.get_club_topics(pars_per_page)
+        expressions = database.get_expressions(topics=data['topic'], limit=20)
+        for index, row in data.iterrows():
+            st.write(f"---")
+            st.markdown(f"<i>{row['topic'].replace('_', ' ')}</i>", unsafe_allow_html=True)
+            st.subheader(f"{row['questions'][2:]}")
+            st.write(f"")
+            st.write(f"{row['answers']}")
+            st.write(f"")
+            exp_list = expressions.query('topic == @row["topic"]')['expressions'].to_list()
+            col1, col2, col3, col4 = st.columns(4)
+            for i, each in enumerate(exp_list):
+                if i % 4 == 0:
+                    col1.markdown(f"<i>{each}</i>", unsafe_allow_html=True)
+                elif i % 4 == 1:
+                    col2.markdown(f"<i>{each}</i>", unsafe_allow_html=True)
+                elif i % 4 == 2:
+                    col3.markdown(f"<i>{each}</i>", unsafe_allow_html=True)
+                else:
+                    col4.markdown(f"<i>{each}</i>", unsafe_allow_html=True)
 
 
 if __name__ == "__main__":
--- db_connection.py
+++ db_connection.py
@@ -7,13 +7,15 @@ class Db:
     def __init__(self):
         self.engine = create_engine(secrets['db_con_string'])
 
-    def get_club_topics(self):
-        return pd.read_sql(f"select * from {secrets['club_topics_table']}", self.engine)
+    def get_club_topics(self, limit):
+        return pd.read_sql(f"SELECT * FROM {secrets['club_topics_table']} ORDER BY rand() LIMIT {limit}",
+                           self.engine)
 
-    def get_expressions(self):
-        return pd.read_sql(f"select * from {secrets['club_expression_table']}", self.engine)
+    def get_expressions(self, topics: list, limit):
+        joining = ", ".join([f"'{x}'" for x in pd.unique(topics)])
+        return pd.read_sql(f"SELECT * FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY topic ORDER BY RAND()) as "
+                           f"rn FROM {secrets['club_expression_table']} WHERE topic IN ({joining})) AS subquery "
+                           f"WHERE rn <= {limit}", self.engine).iloc[:, :-1]
 
     def get_other_chunks(self):
-        return pd.read_sql(f"select * from {secrets['other_chunks_table']}", self.engine)
-
-
+        return pd.read_sql(f"SELECT * FROM {secrets['other_chunks_table']}", self.engine)
